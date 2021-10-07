bde47e4bd8f6f5add95fc438d6ef4de40ef1f81c
ASHUTOSH KUMBHAR <ashutoshkumbhar27@gmail.com>
2021-10-07 18:34:39 +0530

    Improved Quality and Scalability

--- pascalTriangle1.py
+++ pascalTriangle1.py
@@ -1,5 +1,6 @@
 #program to print pascal triangle in O(1)
-n = 5
+n = int(input('Enter Number To Generate Pascal Triangle'))
+print('Pascal Triangle Series Of No',n,'is : ')
 for x in range(n):
   print(" "*(n-x),end=" ")
   print(" ".join(str(11**x)))
